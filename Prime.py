a9a1d3f0281eaf2c04ea6ff8df92a07cce6ed577
shayan6646 <48569691+shayan6646@users.noreply.github.com>
2020-04-22 17:33:21 +0430

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -2,7 +2,7 @@ def prime(num):
 
     if num > 1:
     
-       for i in range(2, num//2): 
+       for i in range(2, num/2): 
   
            if (num % i) == 0: 
                print(num, "is not a prime number") 
