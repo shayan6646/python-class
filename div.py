5a203082891a5fac1522db032375055b1b4cfcbd
shayan6646 <48569691+shayan6646@users.noreply.github.com>
2020-04-22 17:39:34 +0430

    Update div.py

--- div.py
+++ div.py
@@ -1,6 +1,6 @@
 def pos(num1,num2):
 
     if num1%num2==0:
-        print('divisible')
-    elif
-        print('not divisible')
+        print("divisible")
+    elif:
+        print("not divisible")
