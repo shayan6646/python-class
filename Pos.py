9d4c64e3080d654246eab06dbd3da633e86288b3
shayan6646 <48569691+shayan6646@users.noreply.github.com>
2020-04-22 17:52:48 +0430

    Update Pos.py

--- Pos.py
+++ Pos.py
@@ -1,6 +1,8 @@
 def pos(num):
-     j=0   
+     """j=0   
      for i in range(0, num):
                         
-            print(j)
-            j+=1     
+          """  print(j)
+          """  j+=1 
+          
+        print(i)  
