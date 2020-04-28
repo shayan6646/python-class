bd77b680f3ff12c0d008829cc0b8b44fb36ad782
shayan6646 <48569691+shayan6646@users.noreply.github.com>
2020-04-28 13:01:29 +0430

    Add files via upload

---
+++ EX11_2.py
@@ -0,0 +1,14 @@
+class time:
+    def __init__(obj,h,m,s):
+        obj.hour = h
+        obj.minute = m
+        obj.second = s
+    def show(obj,ruz):
+        obj.date = ruz
+        return str(obj.hour)+':'+str(obj.minute)+':'+str(obj.second)
+    def total_number_of_seconds(obj):
+        return str (obj.hour*3600+obj.minute*60+obj.second)
+    def kam_kardan(obj1,hh,mm,ss):
+        obj.hour += hh
+        obj.minute += mm
+        obj.second += ss
