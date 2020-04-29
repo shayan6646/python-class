c1df833fc890f8e20f918a1a6095f1b758844aeb
shayan6646 <48569691+shayan6646@users.noreply.github.com>
2020-04-29 17:49:50 +0430

    Update EX12.py

--- EX12.py
+++ EX12.py
@@ -2,12 +2,18 @@ import tkinter as tk
 
 win=tk.Tk()
 
-lb1=tk.label(win,text='User')
+lb1=tk.Label(win,text='User')
 en1=tk.Entry(win)
 a=en1.get()
 
-lb2=tk.label(win,text='Password')
+lb2=tk.Label(win,text='Password')
 en2=tk.Entry(win)
-b=en1.get()
+b=en2.get()
 
 btn1=tk.Button(win,text='Click here')
+
+lb1.pack()
+lb2.pack()
+en1.pack()
+en2.pack()
+btn1.pack()
