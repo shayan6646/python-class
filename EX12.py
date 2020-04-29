9fd836a9be6ff8c52ea62264fe37ee3666f98a83
shayan6646 <48569691+shayan6646@users.noreply.github.com>
2020-04-29 17:45:41 +0430

    Add files via upload

---
+++ EX12.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+win=tk.Tk()
+
+lb1=tk.label(win,text='User')
+en1=tk.Entry(win)
+a=en1.get()
+
+lb2=tk.label(win,text='Password')
+en2=tk.Entry(win)
+b=en1.get()
+
+btn1=tk.Button(win,text='Click here')
