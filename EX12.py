79a527aa1f711f56209bb039a7a6e4070de1daea
shayan6646 <48569691+shayan6646@users.noreply.github.com>
2020-04-29 18:09:21 +0430

    Update EX12.py

--- EX12.py
+++ EX12.py
@@ -1,5 +1,14 @@
 import tkinter as tk
 
+PASS='12345'
+USER='admin'
+
+def authentication():
+    if PASS==b and USER==a:
+        btn1.config(background ='green') 
+    else:   
+        btn1.config(background ='red') 
+
 win=tk.Tk()
 
 lb1=tk.Label(win,text='User')
@@ -10,10 +19,11 @@ lb2=tk.Label(win,text='Password')
 en2=tk.Entry(win)
 b=en2.get()
 
-btn1=tk.Button(win,text='Click here')
+btn1=tk.Button(win,text='Click here',command = authentication)
 
 lb1.pack()
 lb2.pack()
 en1.pack()
 en2.pack()
 btn1.pack()
+
